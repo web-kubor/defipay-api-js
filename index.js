5cc5b3254c53c31c5ca8ef5508eaebfa37e52166
webkubor <webkubor@163.com>
2022-08-12 14:59:05 +0800

    feat iframe

--- index.js
+++ index.js
@@ -5,9 +5,14 @@ window.defipaySdk.init = (params) => {
     throw "没有初始化的DOM ID";
   }
   let dom = getDom(params.el);
-  console.log(dom, params);
-  dom.style.cssText = "background: #83ADEB; width: 500px; height: 700px;padding: 10px 20px";
-  dom.innerText = "初始化中........"
+  console.log('%c%s', 'color: #00a3cc', "初始化参数", params);
+  dom.style.cssText = "background: #83ADEB; width: 500px; border: 0;";
+  dom.innerText = ""
+  var iframe = document.createElement('iframe'); 
+  iframe.width = "100%"
+  iframe.height = "800px"
+  iframe.src="https://test.defipay.biz/customer/#/fiat/order/Y10LTFT5"; 
+  dom.appendChild(iframe);
 };
 
 function getDom(id) {
