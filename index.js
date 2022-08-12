ea93d722f4706681f5234350a20a1f118fd61c26
webkubor <webkubor@163.com>
2022-08-12 11:38:06 +0800

    处理初始化问题

--- index.js
+++ index.js
@@ -1,7 +1,15 @@
+window.defipaySdk = {};
 
-
-  window.defipaySdk = { }
-
-  window.defipaySdk.init = (params) => {
-  console.log("调试", params);
+window.defipaySdk.init = (params) => {
+  if (!params.el) {
+    throw "没有初始化的DOM ID";
   }
+  let dom = getDom(params.el);
+  console.log(dom, params);
+  dom.style.cssText = "background: #83ADEB; width: 500px; height: 700px;padding: 10px 20px";
+  dom.innerText = "初始化中........"
+};
+
+function getDom(id) {
+  return document.getElementById(id);
+}
