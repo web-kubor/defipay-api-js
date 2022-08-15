740c1afc46b599124505358676d8a59212fab6ff
webkubor <webkubor@163.com>
2022-08-15 11:41:55 +0800

    feat  同步提交调整JS部分

--- dist/defipay-payment.js
+++ dist/defipay-payment.js
@@ -2,27 +2,27 @@ window.defipaySdk = {};
 
 window.defipaySdk.init = (params) => {
   if (!params.el) {
-    callBack(params.httpReturnFailed);
-    throw "没有初始化的DOM ID";
+    callBack(params.httpReturnFailed)
+    throw("没有初始化的DOM ID");
   }
   if (!params.streamingId) {
-    callBack(params.httpReturnFailed);
-    throw "没有流水号";
+    callBack(params.httpReturnFailed)
+    throw("没有流水号");
   }
-  let host = "https://defipay.biz/customer/#/order/";
-  if (params.env === "test") {
-    host = "https://test.defipay.biz/customer/#/order/";
+  let host = "https://defipay.biz/customer/#/order/"
+  if (params.env === "test")  {
+   host = "https://test.defipay.biz/customer/#/order/"
   }
   let dom = getDom(params.el);
-  console.log("%c%s", "color: #00a3cc", "初始化参数", params);
-  dom.style.cssText = "background: #83ADEB; width: 500px; border: 0;";
-  dom.innerText = "";
-  var iframe = document.createElement("iframe");
-  iframe.width = "100%";
-  iframe.height = "1100px";
-  iframe.frameborder = "0";
-  iframe.src = host + params.streamingId;
-  console.log("%c%s", "color: #aa00ff", iframe.src);
+  console.log('%c%s', 'color: #00a3cc', "初始化参数", params);
+  dom.style.cssText = "background: #83ADEB; width: 500px;height:1100px; border: 0;";
+  dom.innerText = ""
+  var iframe = document.createElement('iframe'); 
+  iframe.width = "100%"
+  iframe.height = "100%"
+  iframe.frameborder = "0"
+  iframe.src= host + params.streamingId; 
+  console.log('%c%s', 'color: #aa00ff',  iframe.src);
   dom.appendChild(iframe);
 };
 
@@ -30,7 +30,9 @@ function getDom(id) {
   return document.getElementById(id);
 }
 
+
+
 function callBack(url) {
-  var newTab = window.open("about:blank");
-  newTab.location.href = url;
+  var newTab=window.open('about:blank');
+  newTab.location.href =url;
 }
